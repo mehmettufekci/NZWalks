28b1a7d729820415ab2092960fd022f9d32c558c
mtufekci1446 <mtufekci1446@gmail.com>
2022-11-29 17:25:12 +0300

    Regions Controller, DTOs and Contracts, Automapper and async methods added.

---
+++ src/NZWalks.API/Repositories/RegionRepository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Data;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Repositories
+{
+    public class RegionRepository : IRegionRepository
+    {
+        private readonly NZWalksDbContext _nZWalksDbContext;
+
+        public RegionRepository(NZWalksDbContext nZWalksDbContext)
+        {
+            _nZWalksDbContext = nZWalksDbContext;
+        }
+        public async Task<IEnumerable<Region>> GetAllAsync()
+        {
+           return await _nZWalksDbContext.Regions.ToListAsync();
+        }
+    }
+}
